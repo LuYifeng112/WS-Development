209da67bfb991f3d777a53b4f612f22fc6e1b7ba
Gg-Liu <liugeorge.314@gmail.com>
2022-06-29 21:00:58 +1200

    added readcsv and listtodict functions

---
+++ game/codes/fileSystem/fileReaders.py
@@ -0,0 +1,41 @@
+import csv
+
+def readStatsTuple(id):
+    #returns tuple
+    pass
+
+def readAiTuple(id):
+    #returns tuple
+    pass
+
+def importFromFile(filename):
+    #returns list
+    pass
+
+def readCSVFile(filename):
+    #returns tuple (header, data)
+    #check filename is string
+    assert isinstance(filename, str)
+    
+    #open file
+    file = open(filename)
+    csvReader = csv.reader(file)
+    # header = [] OLD CODE
+    # header = tuple(next(csvReader))
+    data = []
+    for row in csvReader:
+        data.append(tuple(row))
+    file.close()
+    #return list of tuples of data
+    return data
+
+def listToDictionary(aList):
+    #returns Dictionary
+    #Dictionary in format id: data
+    retDict = {}
+    for tupleRow in aList:
+        retDict[tupleRow[0]] = tupleRow[1:]
+
+    return retDict
+
+print(listToDictionary(readCSVFile("game/codes/fileSystem/thisFile.csv")))
